25bbbb521b918cd8f04d97eb5c1fdb41b55d7e80
MuhammedAli21 <mohamedmohamed20211@gmail.com>
2025-03-08 01:38:39 +0200

    adding font , make a widget for card

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(brightness: Brightness.dark),
+      theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
       initialRoute: HomePage.routName,
       routes: {HomePage.routName: (context) => HomePage()},
     );
--- lib/widgets/notes_view_body.dart
+++ lib/widgets/notes_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app_tharawt/widgets/custome_search_icon.dart';
+import 'package:todo_app_tharawt/widgets/notes_card.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -19,46 +20,6 @@ class NotesViewBody extends StatelessWidget {
               ],
             ),
             NotesCard(),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class NotesCard extends StatelessWidget {
-  const NotesCard({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 150,
-      decoration: BoxDecoration(
-        color: Colors.amber,
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            ListTile(
-              contentPadding: EdgeInsets.all(0),
-              title: Text(
-                'Note1',
-                style: TextStyle(color: Colors.black, fontSize: 28),
-              ),
-              subtitle: Text(
-                'Entering date',
-                style: TextStyle(color: Colors.black, fontSize: 16),
-              ),
-              trailing: IconButton(
-                onPressed: () {},
-                icon: Icon(Icons.delete),
-                color: Colors.black,
-              ),
-            ),
-            Text('May, 12', style: TextStyle(color: Colors.black)),
           ],
         ),
       ),
