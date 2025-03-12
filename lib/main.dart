70360b1a04b283da913d62786a53ab2beb7b0087
MuhammedAli21 <mohamedmohamed20211@gmail.com>
2025-03-13 01:41:28 +0200

    init hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:todo_app_tharawt/Constents.dart';
 import 'package:todo_app_tharawt/views/edit_note_page.dart';
 import 'package:todo_app_tharawt/views/home_page.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox(kNotesBox);
   runApp(MyApp());
 }
 
